be9e23ef1b6282180321663cafbded3a12fb0ce4
Gustavo Moura <gustavo.moura@serpro.gov.br>
2024-02-19 15:11:05 -0300

    Melhoramento!

--- src/TesteGitHello.java
+++ src/TesteGitHello.java
@@ -4,6 +4,8 @@ public class TesteGitHello {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello!");
+		System.out.println("Hello 2!");
+		System.out.println("Hello 3!");
 
 	}
 
